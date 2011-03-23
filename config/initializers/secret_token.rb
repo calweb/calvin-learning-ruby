8315f2a96e0e4f9c60c0b7596a17e04770284b07
Calvin Webster <calweb@gmail.com>
2011-03-23 02:21:45 -0400

    putting on da hub so i can access everywhere while im learning ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '46faa5c0140077f0187249c5f962d81c9ed94aa3ce867108502e9e862ddfc8626f0ef70aeae9d4176a18d4162c434f294dd4dd6ed1a7f8042fe8ed086744e845'
