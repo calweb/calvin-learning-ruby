665bbf9fa933c6d11bce780d7ee19a260cc83822
Calvin Webster <calweb@gmail.com>
2011-03-25 01:30:03 -0400

    added some seed data to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,21 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 # seed data for the beginning rails 3 blog tutorial
-user = User.create :email => 'cwebster@dataspringinc.com', :password => 'password'
+user = User.create :email => 'dcumbie@hawkeslearningsystems.com', 
+						 :password => 'guessit',
+						 :password_confirmation => 'guessit'
 Category.create [{:name => 'Programming'},
 					  {:name => 'Event'},
 					  {:name => 'Travel'},
 					  {:name => 'Music'},
 					  {:name => 'TV'}]
+					  
+user.articles.create :title => 'Advanced Active Record',
+						  	:body => "Models need to relate to each other. In the real world, ..",
+						  	:published_at => Date.today
+user.articles.create :title => 'One-to-many associations',
+							:body => "One two three hear me roarrrrr..",
+							:published_at => Date.today
+user.articles.create :title => 'associations',
+							:body => "somethingn that goes in the body",
+							:published_at => Date.today
