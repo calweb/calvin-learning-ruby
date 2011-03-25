52555387326bc69a6909ac1d5d467d6c53c60432
Calvin Webster <calweb@gmail.com>
2011-03-25 00:14:18 -0400

    added custom method to help validate comments as well as a call back function to shoot off an email to the author

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,17 @@
 class Comment < ActiveRecord::Base
 	belongs_to :article
+	
+	validates :name, :email, :body, :presence => true
+	validate :article_should_be_published
+	
+	# custom callback
+	after_create :email_article_author
+	
+	def article_should_be_published
+		errors.add(:article_id, "is not published yet") if article && !article.published?
+	end
+	
+	def after_create
+		puts "We will notify #{article.user.email} later on.."
+	end
 end
